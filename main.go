59dd1e88d1bb6c48d139c4b06a4897c8ef3e3c63
zinirun <heojj97@naver.com>
2021-01-20 14:02:56 +0900

    Add functions

--- main.go
+++ main.go
@@ -4,23 +4,75 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://kr.indeed.com/jobs?q=nodejs&limit=50"
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	pages := getPages()
-	fmt.Println(pages)
+	totalPages := getPages()
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+}
+
+func getPage(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50) // Int to Text
+	fmt.Println("Requesting ", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".jobsearch-SerpJobCard").Each(func(i int, card *goquery.Selection) {
+		extractJob(card)
+	})
+}
+
+func extractJob(card *goquery.Selection) {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".title>a").Text()) //innerText
+	location := cleanString(card.Find(".sjcl").Text())
+	salary := cleanString(card.Find(".salaryText").Text())
+	summary := cleanString(card.Find(".summary").Text())
+	fmt.Println(id, title, location, salary, summary)
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
+
+	defer res.Body.Close()
+
 	doc, err := goquery.NewDocumentFromReader(res.Body)
-	return 0
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+
+	return pages
 }
 
 func checkErr(err error) {
