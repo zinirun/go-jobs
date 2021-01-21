506b345c73dc21fe0e2d17bd3fcc83aaa60eaa2d
zinirun <heojj97@naver.com>
2021-01-21 14:55:08 +0900

    Add GoRoutines

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -18,18 +20,27 @@ type extractedJob struct {
 	summary  string
 }
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+var baseURL string = "https://kr.indeed.com/jobs?q=javascript&limit=50"
+var viewJobURL string = "https://kr.indeed.com/viewjob?jk="
 
 func main() {
+	fmt.Println("Start Exctracting..")
+	c := make(chan []extractedJob)
+	var jobs []extractedJob
 	totalPages := getPages()
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		go getPage(i, c)
 	}
+	for i := 0; i < totalPages; i++ {
+		jobs = append(jobs, <-c...)
+	}
+	writeJobs(jobs)
 }
 
-func getPage(page int) {
+func getPage(page int, c chan<- []extractedJob) {
+	c2 := make(chan extractedJob)
+	var jobs []extractedJob
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50) // Int to Text
-	fmt.Println("Requesting ", pageURL)
 	res, err := http.Get(pageURL)
 	checkErr(err)
 	checkCode(res)
@@ -39,22 +50,25 @@ func getPage(page int) {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	doc.Find(".jobsearch-SerpJobCard").Each(func(i int, card *goquery.Selection) {
-		extractJob(card)
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		go extractJob(card, c2)
 	})
+
+	for i := 0; i < searchCards.Length(); i++ {
+		jobs = append(jobs, <-c2)
+	}
+
+	c <- jobs
 }
 
-func extractJob(card *goquery.Selection) {
+func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 	id, _ := card.Attr("data-jk")
 	title := cleanString(card.Find(".title>a").Text()) //innerText
 	location := cleanString(card.Find(".sjcl").Text())
 	salary := cleanString(card.Find(".salaryText").Text())
 	summary := cleanString(card.Find(".summary").Text())
-	fmt.Println(id, title, location, salary, summary)
-}
-
-func cleanString(str string) string {
-	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+	c <- extractedJob{id: id, title: title, location: location, salary: salary, summary: summary}
 }
 
 func getPages() int {
@@ -73,6 +87,31 @@ func getPages() int {
 	})
 
 	return pages
+}
+
+func writeJobs(jobs []extractedJob) {
+	fmt.Println("Make csv file..")
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush() // 함수가 끝날때 데이터를 입력
+
+	headers := []string{"Link", "Title", "Location", "Salary", "Summary"}
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{viewJobURL + job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
+
+	fmt.Println("Done. Extracted ", len(jobs))
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func checkErr(err error) {
